fec04a25b9ce8741a5968f92025c497961e440f3
Kunihiko Miyanaga <miyanaga@ideamans.com>
2013-10-17 11:09:52 +0900

    First release.

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 mt-shorthand
 ============
 
-Provides short handed template tags. e.g. &lt;mt:h hoge> stands for &lt;mt:var name="hoge" escape="html">.
+Provides short handed template tags. e.g. &lt;mt:h hoge&gt; stands for &lt;mt:var name="hoge"&gt; escape="html">.
+
+## Tags Output Variable
+
+### mt:v
+
+It is short for mt:var.
+You can use attribute 'n' as 'name'.
+
+### mt:e
+
+Alias of mt:v. e means echo.
+
+### mt:h
+
+It if short for mt:var with escape="html".
+
+## Tags Set Variable
+
+### mt:s
+
+s means set.
+Set variables about each attribute.
+
+`&lt;mt:s foo="bar" hoge="fuga"&gt;` sets $foo as 'bar' and $hoge as 'fuga'.
+
+### mt:d
+
+d means defaults.
+Set variables about each attribute if empty.
+
+`&lt;mt:s foo="bar" hoge="fuga"&gt` sets $foo as 'bar' and $hoge as 'fuga' **if undef or empty string**.
+
+### mt:sb
+
+It is short for mt:SetVarBlock.
+
+`&lt;mt:sb foo append="1"&gt;Hello&lt;/mt:sb&gt;` appends 'Hello' to foo.
+
+## Others
+
+### mt:dump
+
+It is a utility tag.
---
+++ plugins/Shorthand/lib/MT/Plugin/Shorthand.pm
@@ -0,0 +1,81 @@
+package MT::Plugin::Shorthand;
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use MT::Util;
+
+sub hdlr_d {
+    my ( $ctx, $args ) = @_;
+
+    foreach my $k ( keys %$args ) {
+        next if $k eq '@';
+        my $v = $ctx->var($k);
+        next if defined($v) && length($v) > 0;
+        $ctx->var($k, $args->{$k});
+    }
+
+    '';
+}
+
+sub hdlr_s {
+    my ( $ctx, $args ) = @_;
+
+    foreach my $k ( keys %$args ) {
+        next if $k eq '@';
+        $ctx->var($k, $args->{$k});
+    }
+
+    '';
+}
+
+sub hdlr_v {
+    my $ctx = shift;
+    my ( $args ) = @_;
+
+    my $name = delete $args->{name} // delete $args->{n};
+    $args->{name} = $name;
+
+    my $res = $ctx->invoke_handler( 'var', @_ );
+    $res;
+}
+
+sub hdlr_h {
+    my ( $ctx, $args ) = @_;
+
+    $args->{escape} = 'html';
+    hdlr_v(@_);
+}
+
+sub hdlr_dump {
+    my ( $ctx, $args ) = @_;
+    my $name = delete $args->{n} // delete $args->{name};
+
+    my $value = $ctx->var($name);
+
+    '<pre>' . Dumper($value) . '</pre>';
+}
+
+sub hdlr_sb {
+    print STDERR "setvarblock", "\n";
+    shift->invoke_handler( 'setvarblock', @_ );
+}
+
+sub hdlr_jsonv {
+    my ( $ctx, $args, $cond ) = @_;
+    my $name = delete $args->{n} // delete $args->{name};
+
+    my $builder = $ctx->stash('builder');
+    my $tokens = $ctx->stash('tokens');
+
+    defined ( my $out = $builder->build($ctx, $tokens) )
+        or return;
+
+    my $json = eval { MT::Util::from_json($out) };
+    $ctx->var( $name, $json );
+
+    '';
+}
+
+1;
